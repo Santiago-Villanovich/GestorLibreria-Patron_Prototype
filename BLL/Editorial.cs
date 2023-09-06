70294b153e6b38e94c8a37a94a499019ac69e7f9
Santiago <santiagovillanovich392@gmail.com>
2023-09-06 19:57:29 -0300

    Add project files.

---
+++ BLL/Editorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Editorial
+    {
+        public string nombre { get; set; }
+        public int cuil { get; set; }
+        public string telefono { get; set; }
+        public string direccion { get; set; }
+
+        public Editorial(int cuil)
+        {
+            this.cuil = cuil;
+        }
+    }
+}
---
+++ BLL/Libro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BLL
+{
+    public class Libro:IPrototipo
+    {
+        public string titulo { get; set; }
+        public int cantHojas { get; set; }
+        public Editorial editorial { get; set; }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ClonProfundo(object ObjProto)
+        {
+            Libro clone = (Libro)this.MemberwiseClone();
+            clone.editorial = new Editorial(editorial.cuil);
+            clone.titulo = String.Copy(titulo);
+            return clone;
+        }
+
+        public object ClonSuperficial(object ObjProto)
+        {
+            return (Libro)this.MemberwiseClone();
+        }
+    }
+}
